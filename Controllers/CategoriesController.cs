c483b8b6c66a047bd0a1eb6fd74398961a182ee4
samsaji01 <59938056+samsaji01@users.noreply.github.com>
2021-02-05 15:03:22 -0500

    Lab 2 has been completed

---
+++ Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(String category)
+        {
+            ViewBag.category = "You selected category: " + category;
+            return View();
+        }
+    }
+}
